83d755f6a01f4a45f876b5f522e3dd941815f5f7
Night221018 <116081107+Night221018@users.noreply.github.com>
2022-12-23 16:49:12 +0800

    Create 2011.cpp

---
+++ 2011.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int ans = 0;
+        for (int i = 0; i < operations.size(); ++i) {
+            for (int j = 0; j < 3; ++j) {
+                if (operations[i][j] == '+')  ++ans;
+                else if (operations[i][j] == '-')  --ans;
+            }
+        }
+        return ans / 2;
+    }
+};
