536129b06610293ea7a802481d54bd4402c3d5b7
Night221018 <116081107+Night221018@users.noreply.github.com>
2023-02-04 23:36:44 +0800

    Create 1145.cpp

---
+++ 1145.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* GetM(TreeNode *root, int x) {
+        if (root == nullptr || root->val == x)  return root;
+        TreeNode *left = GetM(root->left, x);
+        if (left)  return left;
+        TreeNode *right = GetM(root->right, x);
+        return right;
+    }
+
+    int GetC(TreeNode *root) {
+        if (root == nullptr)  return 0;
+        return GetC(root->left) + GetC(root->right) + 1;
+    }
+
+    bool btreeGameWinningMove(TreeNode* root, int n, int x) {
+        TreeNode *my = GetM(root, x); 
+        int a = GetC(my->left);
+        int b = GetC(my->right);
+        int c = GetC(root) - a - b - 1;
+        // cout << a << b << c << endl;
+        if (a > b)  swap(a, b);
+        if (a > c)  swap(a, c);
+        if (b > c)  swap(b, c);
+        
+        return c > a + b;
+    }
+};
