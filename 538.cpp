b65463e08732a27d1ffac91ad7791fd0330b65b5
Night221018 <116081107+Night221018@users.noreply.github.com>
2023-02-07 16:36:39 +0800

    Create 538.cpp

---
+++ 538.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void rebuild(TreeNode *root, int &pre) {
+        if (root == nullptr)  return ;
+        rebuild(root->right, pre);
+        root->val += pre;
+        pre = root->val;
+        rebuild(root->left, pre);
+        return ;
+    }
+    TreeNode* convertBST(TreeNode* root) {
+        int pre = 0;
+        rebuild(root, pre);
+        return root;
+    }
+};
